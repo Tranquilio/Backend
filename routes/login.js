1b0bde7e243b16ad6c4b9adbd1ed2614c46242de
VishalJeyaram <vishalsonic2000@gmail.com>
2023-02-12 14:01:24 +0000

    Lambda connection

--- routes/login.js
+++ routes/login.js
@@ -12,6 +12,9 @@ const dbo = require("../conn");
 const { PRIVATE_KEY, NODEMAILER_EMAIL, NODEMAILER_PASSWORD, EMAIL_VERIFIER_API_KEY } = process.env
 // console.log(process.env)
 
+router.post('/', (req, res, next) => {
+  res.json({ message: "Default message!" })
+})
 
 router.post('/verify-email-address', (req, res, next) => {
   const { email } = req.body
@@ -47,7 +50,7 @@ router.post('/login-init', async (req, res, next) => {
 })
 
 router.post('/login-mongo', async (req, res, next) => {
-  const { email, domain} = req.body
+  const { email, domain } = req.body
   // splits = email.split("@") 
   let db_connect = dbo.getDb();
   result = await db_connect.collection("emails").findOne({ domain: domain });
@@ -70,29 +73,29 @@ router.post('/checkId-unique', async (req, res, next) => {
   const { cid } = req.body
   console.log(cid)
   let db_connect = dbo.getDb();
-  cursor = db_connect.collection("company").find({cid: cid});
+  cursor = db_connect.collection("company").find({ cid: cid });
   cursor.toArray(function (err, result) {
-      if (err) throw err;
-      output = false;
-      if(result.length == 0)
-        output = true
-      res.json(output);
+    if (err) throw err;
+    output = false;
+    if (result.length == 0)
+      output = true
+    res.json(output);
   });
 });
 
 
 router.post('/save-company-info', async (req, res, next) => {
-    let db_connect = dbo.getDb();
-    const {data} = req.body
-    db_connect.collection("company").insertOne(data);
-    res.json({message: "Data has been sent"});
+  let db_connect = dbo.getDb();
+  const { data } = req.body
+  db_connect.collection("company").insertOne(data);
+  res.json({ message: "Data has been sent" });
 });
 
 router.post('/save-profile-info', async (req, res, next) => {
-    let db_connect = dbo.getDb();
-    const {data} = req.body
-    db_connect.collection("user-profile").insertOne(data);
-    res.json({message: "Data has been sent"});
+  let db_connect = dbo.getDb();
+  const { data } = req.body
+  db_connect.collection("user-profile").insertOne(data);
+  res.json({ message: "Data has been sent" });
 });
 
 router.post('/obtain-company-details', async (req, res, next) => {
@@ -102,12 +105,14 @@ router.post('/obtain-company-details', async (req, res, next) => {
   let db_connect = dbo.getDb();
   cursor = db_connect.collection("user-profile").aggregate([
     { $match: { email: email } },
-    { $lookup: {
+    {
+      $lookup: {
         from: "company",
         localField: "cid",
         foreignField: "cid",
         as: "companyinfo"
-      } },
+      }
+    },
     /* { $unwind: "$userinfo" }, */
   ]);
   cursor.toArray(function (err, result) {
@@ -119,7 +124,7 @@ router.post('/obtain-company-details', async (req, res, next) => {
       console.log(result[0])
       res.json(result[0]);
     }
-});
+  });
 });
 
 //Template assessments
