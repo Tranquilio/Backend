52ccd20638071ab70174a061695967b8209b2e2a
VishalJeyaram <vishalsonic2000@gmail.com>
2023-02-23 12:35:06 +0000

    Automated linkage between BE and Lambda

--- routes/login.js
+++ routes/login.js
@@ -12,7 +12,7 @@ const dbo = require("../conn");
 const { PRIVATE_KEY, NODEMAILER_EMAIL, NODEMAILER_PASSWORD, EMAIL_VERIFIER_API_KEY } = process.env
 // console.log(process.env)
 
-router.post('/test-endpoint', (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.json({ message: "Default message for test-endpoint!" })
 })
 
